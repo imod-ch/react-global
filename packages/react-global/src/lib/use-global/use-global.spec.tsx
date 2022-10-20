83e823bcdd2c347422ea0eb8c391fe0224907c44
Dominik Forster <domi@imod.ch>
2022-10-20 14:06:50 +0200

    feat: add getter and setter

--- packages/react-global/src/lib/use-global/use-global.spec.tsx
+++ packages/react-global/src/lib/use-global/use-global.spec.tsx
@@ -1,24 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { renderHook } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 
 import useGlobal from './use-global';
 
 describe('useGlobal', () => {
   beforeEach(() => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (global as any)['fruits'] = ['Apple'];
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (global as any)['name'] = 'useGlobalTest';
   });
 
   it('should return global variable fruits', () => {
     const { result } = renderHook(() => useGlobal<string[]>('fruits'));
 
-    expect(result.current).toEqual(['Apple']);
+    expect(result.current[0]).toEqual(['Apple']);
   });
 
   it('should return global variable fruits', () => {
     const { result } = renderHook(() => useGlobal<string>('name'));
 
-    expect(result.current).toEqual('useGlobalTest');
+    expect(result.current[0]).toEqual('useGlobalTest');
+  });
+
+  it('should set global variable', () => {
+    const { result } = renderHook(() => useGlobal<string>('setterTest'));
+
+    expect((global as any)['setterTest']).toBe(undefined);
+
+    act(() => {
+      result.current[1]('This is a test for setter');
+    });
+
+    expect((global as any)['setterTest']).toBe('This is a test for setter');
   });
 });
--- packages/react-global/src/lib/use-global/use-global.ts
+++ packages/react-global/src/lib/use-global/use-global.ts
@@ -1,6 +1,18 @@
-export const useGlobal = <T = string>(variable: string): T => {
+import { useCallback } from 'react';
+
+export const useGlobal = <T = string>(
+  variable: string
+): [T, (value: T) => void] => {
+  const setValue = useCallback(
+    (value: T) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (global as any)[variable] = value;
+    },
+    [variable]
+  );
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return (global as any)[variable];
+  return [(global as any)[variable], setValue];
 };
 
 export default useGlobal;
